8f7bb3bb87aa1ffd07bdfb82a3ce9fa328f43685
NTD151298 <duongtn1512@gmail.com>
2023-02-09 17:11:46 +0700

    Done this in 09-02-2023

--- c-variables/c-variables.cpp
+++ c-variables/c-variables.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 	cout << "Start!" << " \n";
 	/*	
-	
+
     int - stores integers (whole numbers), without decimals, such as 123 or -123
     double - stores floating point numbers, with decimals, such as 19.99 or -19.99
     char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
@@ -39,5 +39,8 @@ int main() {
     Reserved words (like C++ keywords, such as int) cannot be used as names
 
 	*/
+	const int myNum = 15;  // myNum will always be 15
+	//myNum = 10;   error: assignment of read-only variable 'myNum' 
+	cout << myNum << "\n";
 	return 0;
 }
